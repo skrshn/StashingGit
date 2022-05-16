98973cd9759b5b7a931f27af4aeb677bbb0d3e30
skrshn <skrshn22@gmail.com>
2022-05-16 08:58:54 -1000

    the red color car is ready with some basic features

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         BasicFeatures car = new BasicFeatures();
-
+        car.color = "red";
+        car.doors = "4";
+        car.engineCap = "V8";
+        car.headlights = "white";
+        car.regNo = "12345";
     }
 }
