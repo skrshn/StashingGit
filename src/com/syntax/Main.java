061084773743bb2c4469b9f720159707f1244599
skrshn <skrshn22@gmail.com>
2022-05-16 09:24:37 -1000

    Merge conflict

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -10,9 +10,15 @@ public class Main {
         car.headlights = "white";
         car.regNo = "12345";
 
+<<<<<<< Updated upstream
 
         System.out.println("welcome to my car");
         System.out.println("the radio is set to 80MHZ");
         System.out.println("turn on the A/C");
+=======
+        System.out.println("this is new car");
+        System.out.println("the radio is set to 95MHZ");
+        System.out.println("the sunroof is open");
+>>>>>>> Stashed changes
     }
 }
